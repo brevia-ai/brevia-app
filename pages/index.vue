545170cb714a0857070d4b067a442d6a4587cdf0
xho <xhoxho@gmail.com>
2023-09-27 14:13:09 +0200

    feat: UI big buttons icons

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,18 @@
             <NuxtLink class="big-button flex-row justify-between sm:flex-col sm:justify-center gap-6 lg:gap-10"
                 v-for="(item, i) in menu" :key="i"
                 :to="item.link">
-                <div class="sm:text-center">
+                <div class="grow sm:pt-16 flex flex-col justify-center sm:items-center sm:text-center">
                     <div class="text-5xl leading-tight font-semibold">{{ capitalLetters(item.title, item.type) }}</div>
                     <div class="text-lg lg:text-xl leading-tight">{{ itemTitle(item.title, item.type) }}</div>
                 </div>
 
-                <div class="text-center space-y-1.5 text-sky-400 opacity-80">
-                    <div class="text-xs uppercase tracking-wide">{{ item.type }}</div>
-                    <Icon class="opacity-70" name="carbon:chat-bot" :size="42" v-if="item.type === 'chatbot'" />
-                    <Icon class="opacity-70" name="carbon:search-locate" :size="42" v-else-if="item.type === 'analysis'" />
-                    <Icon class="opacity-70" name="carbon:license-maintenance" :size="42" v-else-if="item.type === 'summary'" />
-                    <Icon class="opacity-70" name="carbon:document-audio" :size="42" v-else-if="item.type === 'transcription'" />
-                    <Icon class="opacity-70" name="carbon:idea" :size="42" v-else />
+                <div class="flex items-center space-x-2 text-sky-300 opacity-90">
+                    <Icon class="opacity-70" name="carbon:chat-bot" :size="28" v-if="item.type === 'chatbot'" />
+                    <Icon class="opacity-70" name="carbon:search-locate" :size="28" v-else-if="item.type === 'analysis'" />
+                    <Icon class="opacity-70" name="carbon:license-maintenance" :size="28" v-else-if="item.type === 'summary'" />
+                    <Icon class="opacity-70" name="carbon:document-audio" :size="28" v-else-if="item.type === 'transcription'" />
+                    <Icon class="opacity-70" name="carbon:idea" :size="28" v-else />
+                    <span class="text-xs uppercase tracking-wider">{{ item.type }}</span>
                 </div>
 
             </NuxtLink>
