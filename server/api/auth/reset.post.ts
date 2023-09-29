0e1b70b6f6c9d78120c71610dbbed6467808833f
Davide <yyzark@gmail.com>
2023-09-29 17:29:17 +0200

    #74 feat:reset-password

---
+++ server/api/auth/reset.post.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(async (event) => {
+    try {
+        const resetUrl = (`${getRequestURL(event).origin}/reset-password`);
+        const reqBody = await readBody(event);
+        const body = {
+            contact: reqBody?.contact,
+            change_url: resetUrl,
+        }
+        const client = beditaApiClient();
+        const response = await client.post('/auth/change', body);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return handleBeditaApiError(event, error);
+    }
+});
