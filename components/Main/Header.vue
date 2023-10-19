bc7b9fa453641262e0ef311bda37df2e45774448
xho <xhoxho@gmail.com>
2023-10-19 13:06:02 +0200

    feat: “Brevia” in navbar

--- components/Main/Header.vue
+++ components/Main/Header.vue
@@ -1,18 +1,18 @@
 <template>
 <header class="py-4 px-6 flex space-x-4 justify-between items-center">
     <div class="flex space-x-2">
-        <img class="mt-2 h-[26px] w-auto" src="/logo.png" alt="Chatlas logo">
+        <img class="mt-2 h-[26px] w-auto" src="/logo.png" alt="Atlas logo">
         <div>
             <h1 class="text-4xl font-bold hover:text-sky-800">
                 <NuxtLink to="/">
-                    <span class="leading-none">CHATLAS</span>
+                    <span class="leading-none">BREVIA</span>
                 </NuxtLink>
             </h1>
             <h2 class="pl-1 leading-none">{{ $t('TITLES.ATLAS_AI_POWERED_TOOLS') }}</h2>
         </div>
     </div>
 
-    <div class="flex space-x-4 md:space-x-5">
+    <div class="flex space-x-3 md:space-x-5">
         <button class="w-10 h-10 button border-none text-xs uppercase"
             v-for="(lang, i) in availableLocales" :key="i"
             @click="setLocale(lang as string)">{{ lang }}</button>
