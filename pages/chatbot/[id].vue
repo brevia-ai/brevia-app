4362da1df65c5519e05caa9ebc9339813a97946e
xho <xhoxho@gmail.com>
2023-10-19 12:31:29 +0200

    feat: megarefactor per usare compposition API di Vue e riscrittura pariale dei metodi

--- pages/chatbot/[id].vue
+++ pages/chatbot/[id].vue
@@ -1,14 +1,14 @@
 <template>
     <main>
-        <div class="text-red-600" v-if="!collectionId">
+        <div class="text-red-600" v-if="!collectionName">
             <p>{{ $t('COLLECTION_NOT_FOUND') }}</p>
         </div>
 
         <div class="space-y-8" v-else>
             <div class="space-y-6">
-                <h2 class="text-2xl md:text-3xl lg:text-4xl leading-tight font-bold">{{ collection.title }}</h2>
+                <h2 class="text-2xl md:text-3xl lg:text-4xl leading-tight font-bold">{{ collection.cmetadata?.title }}</h2>
                 <div class="text-neutral-600"
-                    v-html="collection.description" v-if="collection.description"></div>
+                    v-html="collection.cmetadata?.description" v-if="collection.cmetadata?.description"></div>
             </div>
 
             <div v-if="dialog.length">
@@ -29,13 +29,15 @@
             </div>
 
             <div class="flex space-x-4">
-                <input class="grow text-lg p-2 rounded border border-sky-500 disabled:bg-neutral-100 disabled:border-neutral-300 shadow-md" type="text"
-                    ref="prompt"
-                    :disabled="!isReady"
+                <input type="text"
+                    class="grow text-lg p-2 rounded border border-sky-500 disabled:bg-neutral-100 disabled:border-neutral-300 shadow-md"
+                    ref="input"
+                    v-model.trim="prompt"
+                    :disabled="isBusy"
                     @keydown.enter="submit">
 
                 <button class="px-4 button shadow-md disabled:shadow-none"
-                    :disabled="!isReady"
+                    :disabled="isBusy"
                     @click="submit">
                     <span class="md:hidden">›</span>
                     <span class="hidden md:inline">{{ $t('SEND') }}</span>
@@ -44,7 +46,7 @@
 
             <div class="flex space-x-4">
                 <label class="grow text-lg space-x-2 cursor-pointer">
-                    <input type="checkbox" v-model="sourceDocs" :disabled="isBusy">
+                    <input type="checkbox" v-model="showDocs" :disabled="isBusy">
                     <span>{{ $t('SHOW_DOCUMENTS_FOUND') }}</span>
                 </label>
             </div>
@@ -64,190 +66,170 @@
     </main>
 </template>
 
-<script setup>
-    useHead({ title: 'Chatbot | Chatlas', });
-</script>
+<script lang="ts" setup>
+useHead({ title: 'Chatbot | Chatlas', });
 
-<script>
-import { useRoute } from 'vue-router';
-import { useStatesStore } from '~~/store/states';
+interface DialogItem {
+    who: string;
+    message: string;
+    error: boolean;
+}
 
-export default {
-    data() {
-        return {
-            sessionId: '',
-            collectionId: '',
-            hasCollection: false,
+const { t } = useI18n();
 
-            collection: {},
-            isBusy: false,
-            hasAnswer: false,
-            lastPrompt: '',
-            sourceDocs: false,
-            docs: [],
-            dialog: [],
-            collections: [],
-        }
-    },
+const collection = ref<{name?: string, uuid?: string, cmetadata?: any}>({});
+const isBusy = ref(false);
+const prompt = ref('');
+const input = ref<HTMLElement|null>(null);
+const dialog = ref<DialogItem[]>([]);
+const showDocs = ref(false);
+const docs = ref<any>([]);
 
-    watch: {
-        collectionId() {
-            this.readCollections();
-        }
-    },
+let sessionId = '';
+let collectionName = '';
 
-    created() {
-        const store = useStatesStore();
-        if (process.client) {
-            this.sessionId = self.crypto.randomUUID();
-            const route = useRoute();
-            this.collectionId = route.params.id;
-            store.userAccess(`/chatbot/${this.collectionId}`);
-        }
-        store.readOptions();
-    },
+onBeforeMount(async () => {
+    const route = useRoute();
+    collectionName = route.params.id as string;
 
-    mounted() {
+    // check if user has access to this page (TODO: refactor to use middleware)
+    const store = useStatesStore();
+    store.userAccess(`/chatbot/${collectionName}`);
+
+    // TODO: refactor in store
+    store.readOptions();
+
+    isBusy.value = true;
+    const data = await $fetch(`/api/collections?name=${collectionName}`);
+    collection.value = data.find((x: any) => x.name === collectionName);
+
+    if (!collection.value?.uuid) {
+        throw createError({
+            statusCode: 404,
+            message: 'not found',
+            fatal: true
+        });
+    }
+
+    sessionId = crypto.randomUUID();
+    isBusy.value = false;
+});
+
+watch(isBusy, (val) => {
+    if (!val) {
         setTimeout(() => {
-            this.$refs['prompt']?.focus();
+            input.value?.focus();
         }, 100);
-    },
+    }
+});
 
-    computed: {
-        isReady() {
-            return !this.isBusy && this.hasCollection;
+// methods
+const formatDialogItem = (who: string, message: string, error = false): DialogItem => {
+    return {
+        who,
+        message,
+        error,
+    }
+};
+
+const submit = async () => {
+    if (!prompt.value)
+        return;
+
+    isBusy.value = true;
+
+    dialog.value.push(formatDialogItem('YOU', prompt.value));
+    dialog.value.push(formatDialogItem('CHATLAS', ''));
+
+    const currIdx = dialog.value.length - 1;
+
+    try {
+        await streamingFetchRequest(currIdx);
+        isBusy.value = false;
+    } catch (error) {
+        isBusy.value = false;
+        showErrorInDialog(currIdx);
+        console.log(error);
+    }
+};
+
+const streamingFetchRequest = async (currIdx: number) => {
+    const question = prompt.value;
+    prompt.value = '';
+
+    const response = await fetch('/api/prompt', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'X-Chat-Session': sessionId,
         },
-    },
+        body: JSON.stringify({
+            question,
+            collection: collectionName,
+            source_docs: showDocs.value,
+            streaming: true,
+        }),
+    });
 
-    methods: {
-        async submit() {
-            this.lastPrompt = this.$refs['prompt']?.value?.trim();
-            this.$refs['prompt'].value = '';
-            if (!this.lastPrompt) {
+    const reader = response?.body?.getReader();
+    if (reader) {
+        for await (const chunk of readChunks(reader)) {
+            const text = new TextDecoder().decode(chunk);
+            handleStreamText(text, currIdx);
+        }
+    }
+};
+
+const readChunks = (reader: ReadableStreamDefaultReader) => {
+    return {
+        async* [Symbol.asyncIterator]() {
+            let readResult = await reader.read();
+            while (!readResult.done) {
+                yield readResult.value;
+                readResult = await reader.read();
+            }
+        },
+    };
+};
+
+const handleStreamText = (text: string, currIdx: number) => {
+    if (text.includes('[{"page_content":')) {
+        const idx = text.indexOf('[{"page_content":')
+        dialog.value[currIdx].message += text.slice(0, idx);
+        try {
+            docs.value = JSON.parse(text.slice(idx));
+            if (!showDocs.value)
                 return;
-            }
-            this.isBusy = true;
 
-            this.dialog.push( this.dialogItem('YOU', this.lastPrompt) );
-            this.dialog.push( this.dialogItem('CHATLAS', '') );
-            const currIdx = this.dialog.length - 1;
-
-            try {
-                await this.streamingFetchRequest(currIdx);
-                this.isBusy = false;
-                setTimeout(() => {
-                    this.$refs['prompt'].focus();
-                }, 100);
-            } catch (error) {
-                this.isBusy = false;
-                this.showErrorMessage(currIdx);
-                console.log(error);
-            }
-        },
-
-        async streamingFetchRequest(currIdx) {
-            await fetch('/api/prompt', {
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/json',
-                        'X-Chat-Session': this.sessionId,
-                    },
-                    body: JSON.stringify({
-                        question: this.lastPrompt,
-                        collection: this.collectionId,
-                        source_docs: this.sourceDocs,
-                        streaming: true,
-                    }),
-                }).then(async (response) => {
-                    // response.body is a ReadableStream
-                    const reader = response?.body?.getReader();
-                    for await (const chunk of this.readChunks(reader)) {
-                        const text = new TextDecoder().decode(chunk);
-                        this.handleStreamText(text, currIdx)
-                    }
-                });
-        },
-
-        handleStreamText(text, currIdx) {
-            if (text.includes('[{"page_content":')) {
-                const idx = text.indexOf('[{"page_content":')
-                this.dialog[currIdx].message += text.substr(0, idx);
-                try {
-                    this.docs = JSON.parse(text.substr(idx));
-                    this.logDocs();
-                } catch (e) {
-                    return console.error(e);
-                }
-            } else if (text.startsWith('{"error":')) {
-                try {
-                    const err = JSON.parse(text);
-                    console.error(`Error response from API "${err?.error}"`);
-                    this.showErrorMessage(currIdx);
-                } catch (e) {
-                    return console.error(e);
-                }
-            } else {
-                this.dialog[currIdx].message += text;
-            }
-        },
-
-        logDocs() {
-            if (!this.sourceDocs) {
-                return;
-            }
-
-            this.docs.forEach((doc, i) => {
+            docs.value.forEach((doc: any, i: number) => {
                 console.log('\n\n' + (i + 1) + ').');
                 console.log(doc.page_content);
                 console.log(doc.metadata);
             });
-        },
-
-        readChunks(reader) {
-            return {
-                async* [Symbol.asyncIterator]() {
-                    let readResult = await reader.read();
-                    while (!readResult.done) {
-                        yield readResult.value;
-                        readResult = await reader.read();
-                    }
-                },
-            };
-        },
-
-        async readCollections() {
-            this.isBusy = true;
-            try {
-                const data = await $fetch(`/api/collections?name=${this.collectionId}`);
-                this.collections = data;
-                const coll = this.collections.find(x => x.name === this.collectionId);
-                this.hasCollection = !!coll;
-                console.log(coll);
-                this.collection.title = coll?.cmetadata?.title || '';
-                this.collection.description = coll?.cmetadata?.description || '';
-
-                this.isBusy = false;
-            } catch (error) {
-                this.isBusy = false;
-                console.log(error);
-            }
-        },
-
-        showErrorMessage(index) {
-            if (index) {
-                this.dialog[index] = this.dialogItem('CHATLAS', this.$i18n.t('SOMETHING_WENT_WRONG'), true);
-            } else {
-                this.dialog.push(this.dialogItem('CHATLAS', this.$i18n.t('SOMETHING_WENT_WRONG'), true) );
-            }
-        },
-
-        dialogItem(who, message, error = false) {
-            return {
-                who,
-                message,
-                error,
-            }
-        },
+        } catch (e) {
+            return console.error(e);
+        }
+    } else if (text.startsWith('{"error":')) {
+        try {
+            const err = JSON.parse(text);
+            console.error(`Error response from API "${err?.error}"`);
+            showErrorInDialog(currIdx);
+        } catch (e) {
+            return console.error(e);
+        }
+    } else {
+        dialog.value[currIdx].message += text;
     }
-}
+};
+
+const showErrorInDialog = (index: number) => {
+    const dialogItem = formatDialogItem('CHATLAS', t('SOMETHING_WENT_WRONG'), true);
+
+    if (index) {
+        dialog.value[index] = dialogItem;
+        return;
+    }
+
+    dialog.value.push(dialogItem);
+};
 </script>
