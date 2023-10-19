7d7813a6b128a1a885c7e486573a3242342f844b
xho <xhoxho@gmail.com>
2023-10-19 15:31:54 +0200

    feat: refactor index for composition API and new component for big button

---
+++ components/UIX/BigButton.vue
@@ -0,0 +1,36 @@
+<template>
+<NuxtLink class="big-button flex-row justify-between sm:flex-col sm:justify-center gap-6 lg:gap-10"
+    :to="link">
+
+    <div class="grow sm:pt-16 md:pt-16 flex flex-col justify-center sm:items-center sm:text-center">
+        <div class="text-5xl leading-tight font-semibold">{{ capitalLetters(item.title, item.type) }}</div>
+        <div class="text-lg lg:text-xl leading-tight">{{ itemTitle(item.title, item.type) }}</div>
+    </div>
+
+    <div class="flex items-center space-x-2 text-sky-300 opacity-90">
+        <Icon class="opacity-70" name="carbon:chat-bot" size="28" v-if="item.type === 'chatbot'" />
+        <Icon class="opacity-70" name="carbon:search-locate" size="28" v-else-if="item.type === 'analysis'" />
+        <Icon class="opacity-70" name="carbon:license-maintenance" size="28" v-else-if="item.type === 'summary'" />
+        <Icon class="opacity-70" name="carbon:document-audio" size="28" v-else-if="item.type === 'transcription'" />
+        <Icon class="opacity-70" name="carbon:idea" size="28" v-else />
+        <span class="text-xs uppercase tracking-wider">{{ item.type }}</span>
+    </div>
+
+</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    item: {
+        type: Object,
+        required: true,
+    },
+    link: {
+        type: String,
+        default: '',
+    },
+});
+
+const capitalLetters = (title = '', type = '') => title.charAt(0).toUpperCase() + type.charAt(0).toUpperCase();
+const itemTitle = (title = '', type = '') => title.replace(new RegExp(type, 'gi'), '').trim();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,44 @@
 <template>
     <main>
-        <div class="grid" :class="menu.length > 6? 'grid-dashboard-autofill' : 'grid-dashboard w-full mx-auto max-w-[90rem]'"
+        <div class="grid"
+            :class="menu.length > 6? 'grid-dashboard-autofill' : 'grid-dashboard w-full mx-auto max-w-[90rem]'"
             v-if="menu.length">
-            <NuxtLink class="big-button flex-row justify-between sm:flex-col sm:justify-center gap-6 lg:gap-10"
-                v-for="(item, i) in menu" :key="i"
-                :to="item.link">
-                <div class="grow sm:pt-16 md:pt-16 flex flex-col justify-center sm:items-center sm:text-center">
-                    <div class="text-5xl leading-tight font-semibold">{{ capitalLetters(item.title, item.type) }}</div>
-                    <div class="text-lg lg:text-xl leading-tight">{{ itemTitle(item.title, item.type) }}</div>
-                </div>
 
-                <div class="flex items-center space-x-2 text-sky-300 opacity-90">
-                    <Icon class="opacity-70" name="carbon:chat-bot" size="28" v-if="item.type === 'chatbot'" />
-                    <Icon class="opacity-70" name="carbon:search-locate" size="28" v-else-if="item.type === 'analysis'" />
-                    <Icon class="opacity-70" name="carbon:license-maintenance" size="28" v-else-if="item.type === 'summary'" />
-                    <Icon class="opacity-70" name="carbon:document-audio" size="28" v-else-if="item.type === 'transcription'" />
-                    <Icon class="opacity-70" name="carbon:idea" size="28" v-else />
-                    <span class="text-xs uppercase tracking-wider">{{ item.type }}</span>
-                </div>
-            </NuxtLink>
+            <UIXBigButton v-for="(item, i) in menu" :key="i" :item="item" :link="item.link" />
 
             <button class="big-button place-self-stretch py-6 bg-slate-50 text-slate-700 hover:bg-white hover:text-sky-700"
-                @click="add" v-if="addAvailable">
+                @click="$openModal('create-chatbot')"
+                v-if="isAddEnabled">
                 <Icon name="carbon:add-large" size="64" />
             </button>
         </div>
-        <div v-else>
-            {{ $t('WELCOME') }} {{ statesStore.user?.name }} {{ statesStore.user?.surname }}
+
+        <div class="w-full max-w-lg mx-auto p-16 bg-white rounded space-y-5" v-else>
+            <h1 class="text-lg">Ciao <span class="font-bold">{{ store.user?.name }} {{ store.user?.surname }}</span>,</h1>
+            <div>
+                <p>{{ $t('WELCOME_ON_BREVIA') }}.</p>
+                <p>{{ $t('NO_AI_TOOL_HAS_BEEN_SET_UP_JUST_FOR_YOU') }}.</p>
+            </div>
         </div>
     </main>
 </template>
 
 <script setup>
-    useHead({ title: 'Chatlas', });
-</script>
+useHead({ title: 'Chatlas', });
 
-<script>
-import { useStatesStore } from '~~/store/states';
-import { mapStores } from 'pinia';
+const store = useStatesStore();
+const config = useRuntimeConfig();
+const menu = ref([]);
 
-export default {
-    data() {
-        return {
-            menu: [],
-            maxUserChatbots: null,
-        }
-    },
+const isAddEnabled = computed(() => {
+    const max = Number(config.public?.maxUserChatbots) || null;
+    if (max === null)
+        return true;
 
-    computed: {
-        ...mapStores(useStatesStore),
+    return menu.value.filter((x) => x.type === 'chatbot').length < max;
+});
 
-        addAvailable() {
-            if (!this.maxUserChatbots) {
-                return true;
-            }
-            const collections = this.menu.filter((x) => x.type === 'chatbot');
-
-            return collections?.length < this.maxUserChatbots;
-        }
-    },
-
-    created() {
-        this.menu = this.statesStore.getMenu();
-        const config = useRuntimeConfig();
-        this.maxUserChatbots = config.public?.maxUserChatbots || null;
-    },
-
-    methods: {
-        add() {
-            this.$openModal('create-chatbot');
-        },
-        capitalLetters(title, type) {
-            return (title?.charAt(0)?.toUpperCase() || '') +
-                (type?.charAt(0)?.toUpperCase() || '');
-        },
-        itemTitle(title, type) {
-            return title?.replace(new RegExp(type,'gi'), '')?.trim() || '';
-        },
-    },
-}
+onBeforeMount(async () => {
+    menu.value = store.getMenu();
+});
 </script>
