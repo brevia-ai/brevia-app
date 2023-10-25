9a7f1258a0237b7912fc41fb0ae5629f2d5d8d61
xho <xhoxho@gmail.com>
2023-10-25 18:00:56 +0200

    chore: change var name

--- server/api/bedita/question/[id].delete.ts
+++ server/api/bedita/question/[id].delete.ts
@@ -2,9 +2,9 @@ export default defineEventHandler(async (event) => {
     try {
         const id = getRouterParam(event, 'id');
         const client = await beditaApiClient(event);
-        const responseCollection = await client.delete(`/objects/${id}`);
+        const response = await client.delete(`/objects/${id}`);
 
-        return responseCollection.formattedData;
+        return response.formattedData;
     } catch (error) {
         return handleBeditaApiError(event, error);
     }
