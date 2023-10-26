1183e4c465b55f6424a65efc10f213f3d755b93f
xho <xhoxho@gmail.com>
2023-10-26 16:10:35 +0200

    feat: use a Map in upload api endpoint

--- server/api/bedita/upload/[type].post.ts
+++ server/api/bedita/upload/[type].post.ts
@@ -1,42 +1,43 @@
 import { UploadResourceType } from '@atlasconsulting/bedita-sdk';
 import { MultiPartData } from 'h3';
 
-const getFormData = (name: string, multiPartFormData: MultiPartData[]): MultiPartData | undefined => multiPartFormData.find(item => item.name === name);
+const getFormData = (multiPartFormData: MultiPartData[]) : Map<string |undefined, MultiPartData> => new Map(multiPartFormData.map(item => [item.name, item]));
 
 export default defineEventHandler(async (event) => {
     try {
         const client = await beditaApiClient(event);
-        const body = await readMultipartFormData(event) as MultiPartData[];
+        const formData = await readMultipartFormData(event) as MultiPartData[];
+        const formDataMap = getFormData(formData);
 
-        const fileData = getFormData('file', body);
-        if (!fileData)
+        const file = formDataMap.get('file');
+        if (!file?.data)
             throw new Error('Missing file');
 
-        const file = new File([fileData.data as BlobPart], fileData.filename || '', { type: fileData.type });
+        const fileObject = new File([file.data as BlobPart], file.filename || '', { type: file.type });
         const type: UploadResourceType = getRouterParam(event, 'type') as UploadResourceType;
 
         // upload file
-        const response = await client.upload(file, type);
+        const response = await client.upload(fileObject, type);
         const id = response.data?.data?.id;
 
-        // humanize title
-        const title = getFormData('title', body)?.data.toString();
+        // title and description
+        const title = formDataMap.get('title')?.data.toString();
         if (title) {
             await client.patch(`/${type}/${id}`, {
-                data: {
-                    id,
-                    type,
-                    attributes: {
-                        title,
-                    },
-                },
+                data: { id, type, attributes: { title } }
+            });
+        }
+        const description = formDataMap.get('description')?.data.toString();
+        if (description) {
+            await client.patch(`/${type}/${id}`, {
+                data: { id, type, attributes: { description } }
             });
         }
 
         // relation
-        const relatedId = getFormData('relatedId', body)?.data.toString();
-        const relatedType = getFormData('relatedType', body)?.data.toString();
-        const relationName = getFormData('relationName', body)?.data.toString();
+        const relatedId = formDataMap.get('relatedId')?.data.toString();
+        const relatedType = formDataMap.get('relatedType')?.data.toString();
+        const relationName = formDataMap.get('relationName')?.data.toString();
 
         if (relatedId && relatedType && relationName) {
             await client.post(`/${relatedType}/${relatedId}/relationships/${relationName}`, {
