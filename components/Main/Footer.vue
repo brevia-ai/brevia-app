1075dbe876ea0399a1d7ebcdb4a76c8757dc096d
xho <xhoxho@gmail.com>
2023-10-28 19:05:08 +0200

    feat: minor, footer UI

--- components/Main/Footer.vue
+++ components/Main/Footer.vue
@@ -1,16 +1,16 @@
 <template>
-<footer class="py-28 px-12 flex flex-col items-center bg-gradient-to-b from-slate-900 to-slate-950 space-y-8">
-    <div class="flex space-x-8 text-sky-500">
+<footer class="py-16 sm:py-24 px-12 flex flex-col items-center bg-gradient-to-b from-slate-900 to-slate-950 space-y-4 sm:space-y-8">
+    <div class="flex text-sky-500 text-3xl sm:text-4xl space-x-8">
         <a class="hover:text-sky-300" href="https://it.linkedin.com/company/atlas-srl" target="_blank" rel="noopener">
-            <Icon name="ph:linkedin-logo" class="text-4xl" />
+            <Icon name="ph:linkedin-logo" />
             <span class="sr-only">LinkedIn</span>
         </a>
         <a class="hover:text-sky-300" href="https://atlasconsulting.it/" target="_blank" rel="noopener">
-            <Icon name="ph:globe" class="text-4xl" />
+            <Icon name="ph:globe" />
             <span class="sr-only">Web site Atlas SRL</span>
         </a>
         <a class="hover:text-sky-300" href="https://github.com/atlasconsulting" target="_blank" rel="noopener">
-            <Icon name="ph:github-logo" class="text-4xl" />
+            <Icon name="ph:github-logo" />
             <span class="sr-only">Github</span>
         </a>
     </div>
