e7cd5550d70aba1a707e912a28a8654bf7359798
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-11-27 15:23:58 +0100

    feat: missing endpoints

---
+++ server/api/brevia/service_usage.get.ts
@@ -0,0 +1,22 @@
+const config = useRuntimeConfig()
+
+export default defineEventHandler(async (event) => {
+    // This endpoint is available only in Playground API
+    const url = config.apiBaseUrl + `/service_usage`
+    const query = getQuery(event);
+
+    try {
+        const response: any = await $fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + config.apiSecret,
+            },
+            query,
+        });
+
+        return response;
+    } catch (err: any) {
+        console.log(err);
+        return {error: err?.message || 'Unknown error'};
+    }
+});
