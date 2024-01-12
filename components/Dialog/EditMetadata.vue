6c155e97a14dca6f64184ea10381b60ca78ac9c2
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-01-12 12:37:47 +0100

    feat: dynamic metadata form

--- components/Dialog/EditMetadata.vue
+++ components/Dialog/EditMetadata.vue
@@ -2,8 +2,35 @@
 <div class="space-y-8 leading-tight">
     <div class="text-center space-y-4">
         <p class="text-lg">{{ $t('EDIT_METADATA') }}</p>
-        <!-- <p>{{ $t('EDIT_METADATA_DESCRIPTION') }}</p> -->
     </div>
+
+    <div class="flex flex-col space-y-2.5">
+        <div class="flex flex-col space-y-1" v-for="(meta, name, index) in properties">
+
+            <slot v-if="isSelect(meta)">
+                <label class="flex items-center space-x-2" for="item_{{ index}}">{{ name }}</label>
+                <select name="item_{{ index}}" id="item_{{ index}}" v-model="metadata[name]">
+                    <option v-for="(item, index) in meta.enum" :value="item">{{ item }}</option>
+                </select>
+
+            </slot>
+
+            <slot v-if="isDate(meta)">
+                <label class="flex items-center space-x-2">
+                    <span>{{ name }}</span>
+                    <input type="text" name="item_{{ index}}" id="item_{{ index}}" v-model="metadata[name]">
+                </label>
+            </slot>
+
+            <slot v-if="isCheckbox(meta)">
+                <label class="flex items-center space-x-2">
+                    <span>{{ name }}</span>
+                    <input type="checkbox" name="item_{{ index}}" id="item_{{ index}}" v-model="metadata[name]">
+                </label>
+            </slot>
+        </div>
+    </div>
+
 
     <div class="flex justify-between space-x-4">
         <button class="button button-secondary uppercase" @click="$closeModal">{{ $t('CANCEL') }}</button>
@@ -26,35 +53,39 @@ const props = defineProps({
 
 const statesStore = useStatesStore();
 
+const metadata = ref({});
+
+// documents metadata JSON Schema
+const schema = statesStore.collection?.cmetadata?.documents_metadata || {};
+const properties = schema?.properties || [];
 const error = ref(false);
 
-const newMetadata = async () => {
-    const defaultMeta = await defaultMetadata();
-    const customMeta = customMetadata();
-    return {...defaultMeta, ...customMeta};
-}
-
-const customMetadata = () => {
-    return {custom: 'custom'};
-}
-
-const defaultMetadata = async () => {
-    const meta: Record<string, any> = {type: props.document.type};
-    if (props.document.type === 'files') {
-        try {
-            const data = await $fetch(`/api/bedita/files/${props.document.id}`, { method: 'GET' });
-            meta.file = data.formattedData?.data?.relationships?.streams?.data?.[0]?.attributes?.file_name || '';
-        } catch (err) {
-            console.log(err);
-        }
+onBeforeMount(async () => {
+    try {
+        const response = await fetch(
+            `/api/brevia/index/${statesStore.collection?.uuid}/${props.document.id}`
+        )
+        const data = await response.json();
+        metadata.value = data?.[0]?.cmetadata || {};
+    } catch (error) {
+        console.log(error);
     }
-    console.log(meta);
-
-    return meta;
-}
-
+});
 
 const { $closeModal } = useNuxtApp();
+
+const isSelect = (prop: any) => {
+    return prop?.type == 'string' && prop?.enum;
+}
+
+const isDate = (prop: any) => {
+    return prop?.type == 'string' && prop?.format == 'date';
+}
+
+const isCheckbox = (prop: any) => {
+    return prop?.type == 'boolean';
+}
+
 
 const updateMetadata = async () => {
     try {
@@ -63,7 +94,7 @@ const updateMetadata = async () => {
             body: {
                 collection_id: statesStore.collection?.uuid || '',
                 document_id: props.document.id,
-                metadata: await newMetadata(),
+                metadata: metadata.value,
             },
         });
         $closeModal();
