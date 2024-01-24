1801fc66b6591375cccd55828e693664798634da
batopa <batopa@gmail.com>
2024-01-24 11:29:41 +0100

    refactor: simplify change password

--- components/Dialog/ChangePassword.vue
+++ components/Dialog/ChangePassword.vue
@@ -7,7 +7,7 @@
         <form class="space-y-6" v-if="!success">
             <div>
                 <label for="currentPwd">{{ $t('CURRENT_PASSWORD') }}</label>
-                <input id="currentPwd" type="password" autocomplete='off' class="w-full" v-model="oldPass" @focusout="checkOldPwd"/>
+                <input id="currentPwd" type="password" autocomplete='off' class="w-full" v-model="oldPass" />
             </div>
             <p class="text-red-600 text-sm" v-if="pwdError">{{ $t('PASSWORD_ERROR') }}</p>
             <div class="pt-16">
@@ -18,11 +18,11 @@
                 <label for="confirmPwd">{{ $t('CONFIRM_NEW_PASSWORD') }}:</label>
                 <input id="confirmPwd" type="password" autocomplete='off' class="w-full border-red-300" v-model="confirmPass" />
             </div>
-            <p class="text-red-600 text-sm" v-if="newPass != confirmPass">{{ $t('PASSWORD_MISMATCH') }}</p>
+            <p class="text-red-600 text-sm" v-if="newPass !== confirmPass">{{ $t('PASSWORD_MISMATCH') }}</p>
             <div class="flex justify-between space-x-4">
                 <button class="button button-secondary uppercase" @click="$closeModal">{{ $t('CANCEL') }}</button>
 
-                <button class="button uppercase" :disabled="(newPass != confirmPass || pwdError)" :class="{ 'is-loading': loading }"
+                <button class="button uppercase" :disabled="isButtonDisabled" :class="{ 'is-loading': loading }"
                     @click.prevent.default="changePassword">
                     {{ $t('CHANGE_PASSWORD') }}
                 </button>
@@ -40,68 +40,55 @@
     </div>
 </template>
 
-<script setup>
-    import { useStatesStore } from '~~/store/states';
-    const { $closeModal } = useNuxtApp();
-    const recaptchaInstance = useReCaptcha();
-    const states = useStatesStore();
-    const emit = defineEmits(['stopClick']);
-    const user = states.user;
-    let oldPass = ref('');
-    let newPass = ref('');
-    let confirmPass = ref('');
-    let changeError = ref(false);
-    let pwdError = ref(false);
-    let loading = ref(false);
-    let success = ref(false);
+<script setup lang="ts">
+import { NuxtError } from 'nuxt/app';
 
-    onMounted(() => {
-        emit('stopClick');
-    });
+const { $closeModal } = useNuxtApp();
 
-    const changePassword = async () => {
-        try {
-            loading.value = true;
-            const data = await $fetch('/api/bedita/auth/user', {
-                method: 'PATCH',
-                body: {
-                    newpassword: newPass.value,
-                    oldpassword: oldPass.value,
-                    // recaptcha_token,
-                },
-            });
-            loading.value = false;
-            changeError.value = false;
-            success.value = true;
+const emit = defineEmits(['stopClick']);
 
-        } catch (changeError) {
-            console.log(changeError);
-            success.value = false;
-            loading.value = false;
+let oldPass = ref('');
+let newPass = ref('');
+let confirmPass = ref('');
+let changeError = ref(false);
+let pwdError = ref(false);
+let loading = ref(false);
+let success = ref(false);
+
+const isButtonDisabled = computed(() => {
+    if (oldPass.value.length === 0 || newPass.value.length === 0 || confirmPass.value.length === 0) {
+        return true;
+    }
+
+    return newPass.value !== confirmPass.value;
+})
+
+onMounted(() => {
+    emit('stopClick');
+});
+
+const changePassword = async () => {
+    try {
+        pwdError.value = false
+        changeError.value = false;
+        loading.value = true;
+        await $fetch('/api/bedita/auth/user', {
+            method: 'PATCH',
+            body: {
+                password: newPass.value,
+                old_password: oldPass.value,
+            },
+        });
+        success.value = true;
+    } catch (e: NuxtError | any) {
+        success.value = false;
+        if (['Wrong password', 'Missing current password'].includes(e?.data?.error?.title)) {
+            pwdError.value = true;
+        } else {
             changeError.value = true;
         }
     }
-
-    const checkOldPwd = async () => {
-        try{
-            // Waiting for recaptcha
-            // await recaptchaInstance?.recaptchaLoaded();
-            // const recaptcha = async () => await recaptchaInstance?.executeRecaptcha('login');
-            // const recaptcha_token = await recaptcha();
-
-            const data = await $fetch('/api/bedita/auth/login', {
-                    method: 'POST',
-                    body: {
-                        username: user.username,
-                        password: oldPass.value,
-                        // recaptcha_token,
-                    },
-                });
-            pwdError.value = false
-        } catch (changeError) {
-            console.log(changeError)
-            pwdError.value = true
-        }
-    }
+    loading.value = false;
+}
 
 </script>
