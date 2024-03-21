567c559b781441ee009f3841f0d7061553e92795
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-03-21 12:48:42 +0100

    fix: handle empty chat history

--- pages/chatbot/history-[id].vue
+++ pages/chatbot/history-[id].vue
@@ -47,12 +47,15 @@
                         <option value="CSV">CSV</option>
                         <option value="XSLX">XSLX</option>
                     </select>
-                    <button type="submit" class="button button-primary uppercase" @click="downloadHistory">
+                    <button type="submit" class="button button-primary uppercase" @click="downloadHistory" :disabled="dialog.length === 0">
                         {{ $t('Download') }}
                     </button>
                 </div>
             </div>
-            <div class="flex flex-col space-y-3">
+            <div v-if="!loadingChats && dialog.length === 0" class="flex flex-col space-y-3">
+                <span>{{ $t('CHAT_HISTORY_EMPTY') }}</span>
+            </div>
+            <div v-else class="flex flex-col space-y-3">
                 <div class="flex justify-between border-b border-slate-300">
                     <span>{{ $t('CHAT_HISTORY_PREVIEW') }}</span>
                     <div id="customSelect" class="w-80 px-1 border rounded border-primary bg-white hover:bg-sky-100 focus:outline-primary text-primary  hover:cursor-default">
@@ -74,7 +77,11 @@
                     </div>
                 </div>
                 <div>
-                    <div v-if="dialog.length && !loadingChats">
+                    <div v-if="loadingChats" class="flex flex-row justify-center">
+                        <svg aria-hidden="true" class=" w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                    <div v-else-if="dialog.length">
                         <div class="px-4 pt-6 pb-4 bg-white shadow-md rounded space-y-3">
                             <div class="flex flex-col space-y-6 pb-4">
 
@@ -88,10 +95,6 @@
 
                             </div>
                         </div>
-                    </div>
-                    <div v-else class="flex flex-row justify-center">
-                        <svg aria-hidden="true" class=" w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
-                        <span class="sr-only">Loading...</span>
                     </div>
                 </div>
             </div>
@@ -202,8 +205,11 @@ const downloadXslx = async () => {
 
 const showHistory = async () => {
     historyItems.value = await loadHistoryList();
-    console.log(historyItems.value);
-    let firstChat = historyItems.value[0].sessionId;
+    const firstChat = historyItems.value?.[0]?.sessionId;
+    if (!firstChat) {
+        dialog.value = [];
+        return;
+    }
     selectedChat.value = {
         title: historyItems.value[0].sessionTitle,
         id: historyItems.value[0].sessionId
@@ -213,7 +219,6 @@ const showHistory = async () => {
 
 const loadHistoryList = async () => {
     const items = await loadHistoryItems();
-    console.log("Items",items);
     items.sort((a:any, b:any) => {return +new Date(b.created) - +new Date(a.created);}).reverse();
     const sessions: any[] = [];
     for (let i in items) {
@@ -241,7 +246,7 @@ const loadChat = async(id:any) => {
             loadingChats.value = false;
 
         } catch(error) {
-            console.log(error);
+            console.error(error);
             loadingChats.value = false;
         }
     }
@@ -255,7 +260,7 @@ const loadHistoryItems = async () => {
         const data = await response.json();
         items = data?.data || [];
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 
     return items;
