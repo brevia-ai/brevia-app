b314828f9cf815ee8f7eede655f2b4a8d4a1535e
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-04-23 10:38:32 +0200

    chore: no empty feedback

--- components/Dialog/ShowFeedback.vue
+++ components/Dialog/ShowFeedback.vue
@@ -4,13 +4,12 @@
             <p class="text-lg font-bold">{{ $t('FEEDBACK') }}</p>
             <p class="text-xs">{{ $t('FEEDBACK_SUBTITLE') }}</p>
         </div>
-        <div class="bg-neutral-200 py-2 px-4 rounded-md whitespace-break-spaces">
-            <Icon  v-if="evaluation!==null"
+        <div v-if="evaluation!==null" class="bg-neutral-200 py-2 px-4 rounded-md whitespace-break-spaces" >
+            <Icon
                 class="w-full text-2xl"
                 :class="(evaluation === true)?'text-green-600':'text-red-600'"
                 :name="(evaluation === true)?'material-symbols:thumb-up':'material-symbols:thumb-down'"/>
-            <p v-if="evaluation!==null">{{feedback}}</p>
-            <p v-else>{{ $t('FEEDBACK_NOT_PRESENT') }}</p>
+            <p>{{feedback}}</p>
         </div>
         <div class="w-full flex flex-row">
         <button class="mx-auto button button-primary uppercase" @click="$closeModal">{{ $t('CLOSE') }}</button>
