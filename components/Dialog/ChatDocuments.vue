c5be304ed49e2ec0bf1fcfcf36a1749cdeb67474
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-05-09 18:02:36 +0200

    chore: underscore + padding

--- components/Dialog/ChatDocuments.vue
+++ components/Dialog/ChatDocuments.vue
@@ -8,10 +8,10 @@
             <div class="flex flex-col space-y-3" v-if="documents.length > 0">
                 <div v-for="(doc, n) in documents" :key="n" class="bg-sky-100 shadow-md p-3 rounded-lg ">
                     <p class="text-lg font-bold">{{ $t('DOCUMENT') }} {{ (n + 1) }}.</p>
-                    <p>{{ doc.page_content }}</p>
-                    <p class="font-semibold" v-if="doc.metadata.type">Type: <span class="text-sm font-thin italic">{{ doc.metadata.type }}</span></p>
-                    <p class="font-semibold" v-if="doc.metadata.file">File: <span class="text-sm font-thin italic">{{ doc.metadata.file }}</span></p>
-                    <a class="font-semibold" v-if="doc.metadata.url" :href="doc.metadata.url" target="_blank">Url: <span class="text-sm font-thin italic">{{ doc.metadata.url }}</span></a>
+                    <p class="pt-2 pb-2">{{ doc.page_content }}</p>
+                    <p class="font-semibold" v-if="doc.metadata.type">type: <span class="text-sm font-thin italic">{{ doc.metadata.type }}</span></p>
+                    <p class="font-semibold" v-if="doc.metadata.file">file: <span class="text-sm font-thin italic">{{ doc.metadata.file }}</span></p>
+                    <a class="font-semibold" v-if="doc.metadata.url" :href="doc.metadata.url" target="_blank">url: <span class="text-sm font-thin italic">{{ doc.metadata.url }}</span></a>
                     <div v-if="doc.metadata" v-for="(meta, key) in doc.metadata" :key="key">
                         <p v-if="key.toString() !== 'type' && key.toString() !== 'file' && key.toString() !== 'url'">
                             <span class="font-semibold">{{ key }}:</span> <span class="text-sm font-thin italic">{{ meta }}</span>
