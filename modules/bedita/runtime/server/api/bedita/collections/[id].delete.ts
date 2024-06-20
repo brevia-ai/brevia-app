7e922f47a7909bc3fd8d64ec89d3785712616530
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-06-20 11:12:05 +0200

    feat: update/delete BEdita collection

--- modules/bedita/runtime/server/api/bedita/collections/[id].delete.ts
+++ modules/bedita/runtime/server/api/bedita/collections/[id].delete.ts
@@ -1,10 +1,15 @@
 export default defineEventHandler(async (event) => {
     try {
-        const id = getRouterParam(event, 'id');
+        const path = event.path;
+        const id = path.substring(path.lastIndexOf('/') + 1);
         const client = await beditaApiClient(event);
-        const response = await client.delete(`/collections/${id}`);
+        const response = await client.get(`/collections?filter[collection_uuid]=${id}`);
+        const collectionId = response.data.data?.[0]?.id;
+        if (!collectionId) {
+            return handleBeditaApiError(event, Error('Collection not found'));
+        }
 
-        return response.formattedData;
+        await client.delete(`/collections/${collectionId}`);
     } catch (error) {
         return handleBeditaApiError(event, error);
     }
--- modules/bedita/runtime/server/api/bedita/collections/[id].patch.ts
+++ modules/bedita/runtime/server/api/bedita/collections/[id].patch.ts
@@ -1,12 +1,19 @@
 export default defineEventHandler(async (event) => {
     try {
         const body = await readBody(event);
+        // update only title and description for now
+        const attributes = {
+            title: body.cmetadata.title,
+            description: body.cmetadata.description,
+        }
+        // BEdita ID in cmetadata
+        const id = body.cmetadata.id;
         const client = await beditaApiClient(event);
-        const response = await client.patch(`/collections/${body.id}`, {
+        const response = await client.patch(`/collections/${id}`, {
             data: {
-                id: `${body.id}`,
+                id: `${id}`,
                 type: 'collections',
-                attributes: body.attributes,
+                attributes,
             }
         });
 
