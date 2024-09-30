a1328295893e3983c0d9eb21036993cf406f19a1
Davide Rovai <davide.rovai@atlasconsulting.it>
2024-09-30 15:25:29 +0200

    feat: loader in modal

--- components/Dialog/DocumentChunks.vue
+++ components/Dialog/DocumentChunks.vue
@@ -4,7 +4,7 @@
       <h3 class="text-xl font-semibold text-gray-900">{{ $t('CHUNKS_FOUND') }}</h3>
       <Icon class="text-2xl hover:cursor-pointer hover:bg-sky-100" name="ph:x-bold" @click="$closeModal()" />
     </div>
-    <div class="mb-4 p-2 w-full whitespace-break-spaces overflow-y-auto max-h-[60vh]">
+    <div class="mb-4 p-2 w-full whitespace-break-spaces overflow-y-auto max-h-[60vh]" v-if="!loadingChunks">
       <div v-if="documentChunks && documentChunks.length > 0" class="flex flex-col space-y-3">
         <div v-for="(doc, n) in documentChunks" :key="n" class="bg-sky-100 shadow-md p-3 rounded-lg">
           <p class="text-lg font-bold">CHUNK {{ n + 1 }}.</p>
@@ -14,6 +14,9 @@
       <div v-else class="flex flex-col space-y-3">
         <p class="text-sm font-bold">{{ $t('NO_CHUNKS') }}</p>
       </div>
+    </div>
+    <div class="flex items-center justify-center h-40" v-else>
+      <ElementLoader :loader-dim="80" />
     </div>
   </div>
 </template>
@@ -31,6 +34,7 @@ const statesStore = useStatesStore();
 const { $closeModal } = useNuxtApp();
 const collectionUuid: string = statesStore.collection?.uuid || '';
 const documentChunks = ref<any>([]);
+const loadingChunks = ref(true);
 
 onBeforeMount(async () => {
   try {
@@ -39,8 +43,10 @@ onBeforeMount(async () => {
     if (data) {
       documentChunks.value = data.sort((a: any, b: any) => a.cmetadata.part - b.cmetadata.part);
     }
+    setTimeout(() => loadingChunks.value = false, 250);
   } catch (err) {
     console.log(err);
+    setTimeout(() => loadingChunks.value = false, 250);
   }
 });
 
