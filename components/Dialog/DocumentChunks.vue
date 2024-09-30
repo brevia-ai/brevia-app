f755958d72925ce3ca4296ebab5d42c0ccb4f0a3
Davide Rovai <davide.rovai@atlasconsulting.it>
2024-09-30 15:33:08 +0200

    Feat: adding chunks count and renaming chunks in parts

--- components/Dialog/DocumentChunks.vue
+++ components/Dialog/DocumentChunks.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full">
     <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
-      <h3 class="text-xl font-semibold text-gray-900">{{ $t('CHUNKS_FOUND') }}</h3>
+      <h3 class="text-xl font-semibold text-gray-900">{{ $t('CHUNKS_FOUND', { chunks: documentChunks.length } ) }}</h3>
       <Icon class="text-2xl hover:cursor-pointer hover:bg-sky-100" name="ph:x-bold" @click="$closeModal()" />
     </div>
     <div class="mb-4 p-2 w-full whitespace-break-spaces overflow-y-auto max-h-[60vh]" v-if="!loadingChunks">
       <div v-if="documentChunks && documentChunks.length > 0" class="flex flex-col space-y-3">
         <div v-for="(doc, n) in documentChunks" :key="n" class="bg-sky-100 shadow-md p-3 rounded-lg">
-          <p class="text-lg font-bold">CHUNK {{ n + 1 }}.</p>
+          <p class="text-lg font-bold">{{ $t('CHUNK') }} {{ n + 1 }}.</p>
           <p class="pt-2 pb-2">{{ doc.document }}</p>
         </div>
       </div>
