2fc88cbca70c9ae25ff62fcaa6dc124f04c0ce49
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-10-01 11:48:28 +0200

    chore: document chunks model labels update

--- components/Dialog/DocumentChunks.vue
+++ components/Dialog/DocumentChunks.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full">
     <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
-      <h3 class="text-xl font-semibold text-gray-900">{{ $t('CHUNKS_FOUND', { chunks: documentChunks.length }) }}</h3>
+      <h3 class="text-xl font-semibold text-gray-900">{{ fileName }} {{ $t('CHUNKS_FOUND', { chunks: documentChunks.length }) }}</h3>
       <Icon class="text-2xl hover:cursor-pointer hover:bg-sky-100" name="ph:x-bold" @click="$closeModal()" />
     </div>
     <div class="mb-4 p-2 w-full whitespace-break-spaces overflow-y-auto max-h-[60vh]" v-if="!loadingChunks">
       <div v-if="documentChunks && documentChunks.length > 0" class="flex flex-col space-y-3">
         <div v-for="(doc, n) in documentChunks" :key="n" class="bg-sky-100 shadow-md p-3 rounded-lg">
-          <p class="text-lg font-bold">{{ $t('CHUNK') }} {{ n + 1 }}.</p>
+          <p class="text-lg font-bold">{{ $t('CHUNK') }} {{ n + 1 }}. - {{ $t('NUM_CHARS', { num: doc.document.length}) }}</p>
           <p class="pt-2 pb-2">{{ doc.document }}</p>
         </div>
       </div>
@@ -35,6 +35,7 @@ const { $closeModal } = useNuxtApp();
 const collectionUuid: string = statesStore.collection?.uuid || '';
 const documentChunks = ref<any>([]);
 const loadingChunks = ref(true);
+const fileName = ref('');
 
 onBeforeMount(async () => {
   try {
@@ -42,6 +43,12 @@ onBeforeMount(async () => {
     const data = await response.json();
     if (data) {
       documentChunks.value = data.sort((a: any, b: any) => a.cmetadata.part - b.cmetadata.part);
+      const type = data?.[0]?.cmetadata?.type || '';
+      if (type === 'files') {
+        fileName.value = data?.[0]?.cmetadata?.file || '';
+      } else if (type === 'links') {
+        fileName.value = data?.[0]?.cmetadata?.url || '';
+      }
     }
     setTimeout(() => (loadingChunks.value = false), 250);
   } catch (err) {
