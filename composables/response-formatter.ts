da3a2fd0c3e6ec6d0a67747ce4ab6a2c27fc7ab6
Davide Rovai <davide.rovai@atlasconsulting.it>
2024-10-15 11:54:34 +0200

    feat: markdown recognizes lists now

--- composables/response-formatter.ts
+++ composables/response-formatter.ts
@@ -4,7 +4,8 @@ const converter = new showdown.Converter();
 export const useResponseFormat = () => {
   const formatResponse = (textToFormat: string, format: string = 'text') => {
     if (format === 'markdown') {
-      return converter.makeHtml(textToFormat);
+      const indentedText = recognizeLists(textToFormat);
+      return converter.makeHtml(indentedText);
     }
 
     return textToFormat;
@@ -17,6 +18,24 @@ export const useResponseFormat = () => {
 
     return 'text';
   };
+
+  const recognizeLists = (input: string) => {
+    // This regex captures all lines that start with ordered lists (N. ) or unordered lists (- )
+    const listRegex = /^(\s*)([\d]+\.)?(-)?\s(.*)$/m;
+
+    const lines = input.split('\n');
+    const indentedLines = lines.map(line => {
+      console.log("Linea: ", line);
+      return line.replace(listRegex, (_, spaces, bulletLi, bulletUl, content) => {
+        const bullet = (bulletLi !== undefined) ? bulletLi : bulletUl;
+        // If the spaces are not multiple of 4 we change them to be so instead(Showdownjs uses 4 spaces for lists)
+        const mod4 = spaces.length%4;
+        const newIndentation = ' '.repeat(( mod4 != 0) ? mod4*4 : 0);
+        return `${newIndentation}${bullet} ${content}`;
+      });
+    });
+    return indentedLines.join('\n');
+  }
 
   return {
     llmResponseFormat,
