979969dbb693a4e88d7b6ae515aa29d3467cecfa
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-11-12 19:21:54 +0100

    chore: add div + classes

--- components/Dashboard/Menu.vue
+++ components/Dashboard/Menu.vue
@@ -4,10 +4,12 @@
 
     <button
       v-if="addEnabled"
-      class="big-button place-self-stretch py-6 from-slate-50 to-slate-50 text-slate-700 hover:from-white hover:to-white hover:text-sky-500"
+      class="big-button relative flex-row justify-between sm:flex-col sm:justify-center gap-1 md:gap-0 place-self-stretch py-6 from-slate-50 to-slate-50 text-slate-700 hover:from-white hover:to-white hover:text-sky-500"
       @click="$openModal('DialogNewChatbot')"
     >
-      <Icon name="ph:plus-bold" size="64" />
+      <div class="grow md:pb-3 flex flex-col justify-center">
+        <Icon name="ph:plus-bold" size="64" />
+      </div>
       <div class="flex items-center space-x-2">
         <Icon class="opacity-70" name="carbon:chat-bot" size="28" />
         <span class="text-xs uppercase tracking-wider">chatbot</span>
