fdd1c4888fecd9f8d43404d7c55e0789a3c18758
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-12-11 12:55:13 +0100

    refactor: FormLogin component

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,66 +1,8 @@
 <template>
-  <main class="mt-16">
-    <div v-if="!statesStore.isLogged()" class="mt-6 max-w-sm mx-auto space-y-8">
-      <form class="flex flex-col space-y-8" @submit.stop.prevent>
-        <select v-if="availableProjects.length > 0" v-model="projectSelected"
-            class="border rounded border-primary bg-white hover:bg-sky-100 focus:outline-primary text-primary text-xl h-14 px-4">
-          <option v-for="item in availableProjects" :key="item" :value="item" class="px-4">{{ item }}</option>
-        </select>
-
-        <UIXInput
-          v-model="username"
-          autocomplete="username"
-          autocorrect="off"
-          autocapitalize="none"
-          :placeholder="$t('LOGIN_PLACEHOLDER')"
-          required
-          @keydown.enter.stop.prevent="signIn"
-        />
-
-        <div class="flex flex-col space-y-3">
-          <UIXInput
-            v-model="password"
-            password
-            autocomplete="current-password"
-            autocorrect="off"
-            autocapitalize="none"
-            :placeholder="$t('PASSWORD_PLACEHOLDER')"
-            required
-            @keydown.enter.stop.prevent="signIn"
-          />
-
-          <NuxtLink v-if="features.changePassword" to="/forgot-password" class="text-xs text-end mt-0 pt-0 text-sky-600">{{ $t('FORGOT_PASS') }}</NuxtLink>
-        </div>
-
-        <button class="p-4 button text-lg" :class="{ 'is-loading': isLoading }" :disabled="!loginEnabled" @click="signIn">{{ $t('SIGN_IN') }}</button>
-
-        <div v-if="features.signup" class="text-sm text-center">
-          {{ $t('NOT_A_MEMBER') }}
-          <NuxtLink class="text-sky-800" to="/signup">{{ $t('SIGN_UP_HERE') }}</NuxtLink>
-        </div>
-      </form>
-      <div v-if="error" class="w-full bg-red-100 border border-red-400 rounded text-center">
-        {{ $t('WRONG_CREDENTIALS') }}
-      </div>
-
-      <div v-if="features.privacyDocuments" class="text-xs text-center text-neutral-400">
-        <i18n-t keypath="RECAPTCHA" tag="p">
-          <template #privacyPolicy>
-            <a class="font-semibold" href="https://policies.google.com/privacy">{{ $t('PRIVACY_POLICY') }}</a>
-          </template>
-          <template #termsOfService>
-            <a class="font-semibold" href="https://policies.google.com/terms">{{ $t('TERMS_OF_SERVICE') }}</a>
-          </template>
-        </i18n-t>
-      </div>
-    </div>
-  </main>
+  <FormLogin :available-projects="availableProjects" />
 </template>
 
 <script setup lang="ts">
-const statesStore = useStatesStore();
-const { login } = useIntegrationAuth();
-
 definePageMeta({
   middleware: [
     function () {
@@ -72,16 +14,7 @@ definePageMeta({
   ],
 });
 
-const error = ref(false);
-const username = ref('');
-const password = ref('');
-const isLoading = ref(false);
-const features = useIntegrationFeatures();
-const projectSelected = ref('');
 const availableProjects = await $fetch('/api/projects');
-const loginEnabled = computed(() => {
-  return username.value && password.value && (availableProjects.length == 0 || projectSelected.value);
-});
 
 onBeforeMount(() => {
   const config = useRuntimeConfig();
@@ -93,16 +26,4 @@ onBeforeMount(() => {
     });
   }
 });
-
-const signIn = async () => {
-  isLoading.value = true;
-  error.value = false;
-  try {
-    await login(username.value, password.value, projectSelected.value);
-    await navigateTo('/');
-  } catch (e) {
-    error.value = true;
-  }
-  isLoading.value = false;
-};
 </script>
--- pages/project-login/[id].vue
+++ pages/project-login/[id].vue
@@ -1,68 +1,8 @@
 <template>
-  <main class="mt-16">
-    <div v-if="!statesStore.isLogged()" class="mt-6 max-w-sm mx-auto space-y-8">
-      <form class="flex flex-col space-y-8" @submit.stop.prevent>
-        <div>
-          <span class="text-xl h-14 px-4">
-            {{ projectSelected }} {{ $t('LOGIN') }}
-          </span>
-
-        </div>
-
-        <UIXInput
-          v-model="username"
-          autocomplete="username"
-          autocorrect="off"
-          autocapitalize="none"
-          :placeholder="$t('LOGIN_PLACEHOLDER')"
-          required
-          @keydown.enter.stop.prevent="signIn"
-        />
-
-        <div class="flex flex-col space-y-3">
-          <UIXInput
-            v-model="password"
-            password
-            autocomplete="current-password"
-            autocorrect="off"
-            autocapitalize="none"
-            :placeholder="$t('PASSWORD_PLACEHOLDER')"
-            required
-            @keydown.enter.stop.prevent="signIn"
-          />
-
-          <NuxtLink v-if="features.changePassword" to="/forgot-password" class="text-xs text-end mt-0 pt-0 text-sky-600">{{ $t('FORGOT_PASS') }}</NuxtLink>
-        </div>
-
-        <button class="p-4 button text-lg" :class="{ 'is-loading': isLoading }" :disabled="!loginEnabled" @click="signIn">{{ $t('SIGN_IN') }}</button>
-
-        <div v-if="features.signup" class="text-sm text-center">
-          {{ $t('NOT_A_MEMBER') }}
-          <NuxtLink class="text-sky-800" to="/signup">{{ $t('SIGN_UP_HERE') }}</NuxtLink>
-        </div>
-      </form>
-      <div v-if="error" class="w-full bg-red-100 border border-red-400 rounded text-center">
-        {{ $t('WRONG_CREDENTIALS') }}
-      </div>
-
-      <div v-if="features.privacyDocuments" class="text-xs text-center text-neutral-400">
-        <i18n-t keypath="RECAPTCHA" tag="p">
-          <template #privacyPolicy>
-            <a class="font-semibold" href="https://policies.google.com/privacy">{{ $t('PRIVACY_POLICY') }}</a>
-          </template>
-          <template #termsOfService>
-            <a class="font-semibold" href="https://policies.google.com/terms">{{ $t('TERMS_OF_SERVICE') }}</a>
-          </template>
-        </i18n-t>
-      </div>
-    </div>
-  </main>
+  <FormLogin :login-project="loginProject" />
 </template>
 
 <script setup lang="ts">
-const statesStore = useStatesStore();
-const { login } = useIntegrationAuth();
-
 definePageMeta({
   middleware: [
     function () {
@@ -74,40 +14,16 @@ definePageMeta({
   ],
 });
 
-const error = ref(false);
-const username = ref('');
-const password = ref('');
-const isLoading = ref(false);
-const features = useIntegrationFeatures();
-const projectSelected = ref('')
-const loginEnabled = computed(() => {
-  return username.value && password.value;
-});
-
-onBeforeMount(async () => {
-  const availableProjects = await $fetch('/api/projects');
-  const route = useRoute();
-  const project = (route.params.id as string).toLowerCase().replace(/ /g, '-');
-  projectSelected.value = availableProjects.find((p: string) => p.toLowerCase().replace(/ /g, '-') === project);
-  const config = useRuntimeConfig();
-  if (!projectSelected.value || !!config.public.projectLogin === false) {
-    throw createError({
-      statusCode: 404,
-      message: 'not found',
-      fatal: true,
-    });
-  }
-});
-
-const signIn = async () => {
-  isLoading.value = true;
-  error.value = false;
-  try {
-    await login(username.value, password.value, projectSelected.value);
-    await navigateTo('/');
-  } catch (e) {
-    error.value = true;
-  }
-  isLoading.value = false;
-};
+const availableProjects = await $fetch('/api/projects');
+const route = useRoute();
+const project = (route.params.id as string).toLowerCase().replace(/ /g, '-');
+const loginProject:string | null = availableProjects.find((p: string) => p.toLowerCase().replace(/ /g, '-') === project);
+const config = useRuntimeConfig();
+if (!loginProject || !!config.public.projectLogin === false) {
+  throw createError({
+    statusCode: 404,
+    message: 'not found',
+    fatal: true,
+  });
+}
 </script>
