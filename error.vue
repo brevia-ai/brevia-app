6b78be6c63e774d080a02b0af56e8b68df8cfa1e
Davide Rovai <davide.rovai@atlasconsulting.it>
2025-01-14 17:51:17 +0100

    Fix: reading message from error object

--- error.vue
+++ error.vue
@@ -3,9 +3,12 @@
     <div class="mx-auto space-y-2 place-items-center">
       <img alt="error-image" src="./public/brevia-logo-shatter.png" class="h-36" />
       <div class="text-7xl md:text-9xl font-bold">OPS! {{ error.statusCode }}</div>
-      <div>{{ getErrorMessage() }}</div>
+      <div>{{ error.message }}</div>
     </div>
-    <NuxtLink v-if="!error.data?.noBackHome" to="/" class="button mx-auto mt-8"> Torna alla home </NuxtLink>
+    <NuxtLink v-if="!error.data?.noBackHome" to="/" class="button mx-auto mt-8">
+      <Icon name="ph:house-line-fill" size="28" />
+      <span>Home</span>
+    </NuxtLink>
   </div>
 </template>
 
@@ -15,17 +18,4 @@ import type { NuxtError } from '#app';
 const props = defineProps({
   error: Object as () => NuxtError,
 });
-
-const getErrorMessage = () => {
-  switch (props.error.statusCode) {
-    case 401:
-      return 'Non si dispone delle autorizzazioni necessarie per visitare questa pagina';
-    case 403:
-      return 'Non si dispone delle autorizzazioni necessarie per visitare questa pagina';
-    case 404:
-      return 'Pagina non trovata!';
-    case 500:
-      return 'Errore server, contattare il supporto';
-  }
-};
 </script>
