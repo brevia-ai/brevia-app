683006f04404892cfc1b506b15db8a02b133a0aa
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2025-01-27 11:08:50 +0100

    Handle `date-time` and plain `string` types in metadata (#30)
    * feat: add `date-time` and plain `string` types
    
    * feat: format date with moment + enable time picker
    
    * fix: formatting dates before saving
    
    * chore: changed 'let' to 'const' to make eslint happy
    
    * fix: property saving to predefined fields and improve text input
    
    * fix: prettier
    
    * fix: adding formatting for ISO date-time on onMount and removing check for simple Date
    
    * fix: format item if not nulll
    
    ---------
    
    Co-authored-by: Davide Rovai <davide.rovai@atlasconsulting.it>

--- components/Dialog/EditMetadata.vue
+++ components/Dialog/EditMetadata.vue
@@ -14,7 +14,7 @@
       </div>
 
       <div v-else class="flex flex-col space-y-2.5">
-        <div v-for="(meta, name) in properties" :key="name" class="mx-12 content-start grid grid-cols-2 gap-2">
+        <div v-for="(meta, name) in properties" :key="name" class="mx-8 content-start grid grid-cols-2 gap-2">
           <slot v-if="isSelect(meta)">
             <label class="items-center space-x-2 justify-end py-1" for="item_{{ index}}">{{ name }}</label>
             <select id="item_{{ index}}" v-model="metadata[name]" class="justify-self-start border rounded border-slate-300 p-1" name="item_{{ index}}">
@@ -37,6 +37,30 @@
               :format="formatDate"
             >
             </VueDatePicker>
+          </slot>
+
+          <slot v-if="isDateTime(meta)">
+            <label class="flex items-center space-x-2">
+              <span>{{ name }}</span>
+            </label>
+            <VueDatePicker
+              v-model="metadata[name]"
+              :range="false"
+              :enable-time-picker="true"
+              time-picker-inline
+              text-input
+              position="center"
+              model-type="yyyy-MM-dd HH:mm:ss"
+              :format="formatDateTime"
+            >
+            </VueDatePicker>
+          </slot>
+
+          <slot v-if="isSimpleString(meta)">
+            <label class="my-auto">
+              <span>{{ name }}</span>
+            </label>
+            <input id="item_{{ index}}" v-model="metadata[name]" type="text" name="item_{{ index}}" class="max-h-10" />
           </slot>
 
           <slot v-if="isCheckbox(meta)">
@@ -68,6 +92,8 @@
 <script lang="ts" setup>
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
+import moment from 'moment';
+
 const props = defineProps({
   document: {
     type: Object,
@@ -92,6 +118,11 @@ onBeforeMount(async () => {
     const data = await response.json();
     docsFound.value = (data?.length || 0) > 0;
     metadata.value = data?.[0]?.cmetadata || {};
+    Object.entries(metadata.value).forEach(([k, v]) => {
+      if (isDateTime(properties[k])) {
+        metadata.value[k] = moment(v as any).format('yyyy-MM-DD HH:mm:ss');
+      }
+    });
     setTimeout(() => (loaded.value = true), 250);
   } catch (err) {
     console.log(err);
@@ -109,18 +140,34 @@ const isDate = (prop: any) => {
   return prop?.type == 'string' && prop?.format == 'date';
 };
 
+const isDateTime = (prop: any) => {
+  return prop?.type == 'string' && prop?.format == 'date-time';
+};
+
+const isSimpleString = (prop: any) => {
+  return prop?.type == 'string' && !prop?.format && !prop?.enum;
+};
+
 const isCheckbox = (prop: any) => {
   return prop?.type == 'boolean';
 };
 
 const updateMetadata = async () => {
+  const formattedMetadata: typeof metadata.value = {};
+  Object.entries(metadata.value).forEach(([k, v]) => {
+    formattedMetadata[k] = v;
+    if (isDateTime(properties[k]) && !!v) {
+      formattedMetadata[k] = new Date(v as string).toISOString();
+    }
+  });
+
   try {
     await $fetch('/api/brevia/index/metadata', {
       method: 'POST',
       body: {
         collection_id: statesStore.collection?.uuid || '',
         document_id: props.document.custom_id,
-        metadata: metadata.value,
+        metadata: formattedMetadata,
       },
     });
     $closeModal();
@@ -134,10 +181,15 @@ const formatDate = (date: any) => {
   if (!date) {
     return ``;
   }
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
 
-  return `${day}/${month}/${year}`;
+  return moment(date).format('DD/MM/YYYY');
+};
+
+const formatDateTime = (date: any) => {
+  if (!date) {
+    return ``;
+  }
+
+  return moment(date).format('DD/MM/YYYY HH:mm');
 };
 </script>
