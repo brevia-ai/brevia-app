948bc9a72b8f8d70235eaf4f84e2d0757b6685b7
BuonDavide <davide.rovai@atlasconsulting.it>
2025-05-06 11:19:21 +0200

    feat/chat-senza-rag

---
+++ pages/no-rag/test-chat.vue
@@ -0,0 +1,129 @@
+<template>
+  <main>
+    <div class="space-y-12">
+      <div class="flex justify-between items-start space-x-4">
+        <div class="space-y-4">
+          <h2 class="text-2xl md:text-3xl leading-tight font-bold">Test Chat</h2>
+          <div class="text-neutral-600">Chat di test senza rag</div>
+        </div>
+        <div class="flex justify-between space-x-4">
+          <div class="mt-0.5 text-sky-700 hover:text-sky-500 hover:cursor-pointer">
+            <Icon name="ph:arrow-clockwise-bold" class="text-4xl" @click="chatZone.refreshChat()" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2 class="relative text-lg md:text-xl leading-tight font-bold mb-6 border-b-2" @click="showHideConfig()">
+          Configura il tuo modello
+          <button class="absolute right-0">
+            <Icon :name="configVisible ? 'ph:caret-up' : 'ph:caret-down'" />
+          </button>
+        </h2>
+        <Transition name="section-fade">
+          <div v-if="configVisible">
+            <ConfigLlm v-model="qaFollowupLLm" title="Followup LLM" />
+          </div>
+        </Transition>
+      </div>
+      <ChatZone
+        ref="chatZone"
+        :collection
+        :is-full-page="false"
+        :max-messages="maxChatMessages"
+        bot-name="test-chat"
+        @update-left="updateLeftMessages"
+        @feedback="(feed) => handleFeedback(feed)"
+      >
+        <template #extra-icons>
+          <div v-if="chatZone.showLastMsgDocs" class="px-1.5 pb-1 hover:bg-neutral-500 hover:rounded-md cursor-pointer">
+            <Icon name="ph:files-bold" class="text-white" @click="$openModal('ChatDocuments', { documents: chatZone.getResponseDocs() })" />
+          </div>
+        </template>
+        <template #messageCounter>
+          <div v-if="isDemo" class="flex space-x-4">
+            <span class="grow text-lg">{{ $t('MESSAGES_LEFT') }}: {{ messagesLeft }}</span>
+          </div>
+
+          <div v-if="isDemo && messagesLeft == 0" class="space-x-4">
+            <div class="w-full bg-red-100 border border-red-400 rounded text-center">
+              {{ $t('NO_MORE_CHAT_MESSAGES') }}
+            </div>
+          </div>
+        </template>
+      </ChatZone>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+const store = useStatesStore();
+const modalStore = useModalStore();
+useHead({ title: `Chatbot | ${config.public.appName}` });
+
+const chatZone = ref();
+const collection = ref<{ name?: string; uuid?: string; cmetadata?: any }>({});
+const isBusy = ref(false);
+const input = ref<HTMLElement | null>(null);
+const isDemo = ref(store.userHasRole('demo'));
+const maxChatMessages = isDemo.value ? parseInt(config.public.demo.maxChatMessages) : 0;
+const messagesLeft = ref(maxChatMessages);
+
+const configVisible = ref(true);
+const breviaConfig: any = await $fetch('/api/brevia/config');
+const qaFollowupLLm = ref(breviaConfig.qa_followup_llm);
+
+onBeforeMount(async () => {
+  isBusy.value = true;
+  isBusy.value = false;
+});
+
+watch(isBusy, (val) => {
+  if (!val) {
+    setTimeout(() => {
+      input.value?.focus();
+    }, 100);
+  }
+});
+
+const showHideConfig = () => (configVisible.value = !configVisible.value);
+
+const updateLeftMessages = (left: number) => (messagesLeft.value = left);
+
+const handleFeedback = (feed: any) => {
+  modalStore.openModal('GiveFeedback', { feedback: feed });
+  modalStore.$onAction(({ after, onError }) => {
+    after((result) => {
+      if (result === 'refreshFeedback') {
+        chatZone.value.updateFeedbackThumbs();
+      }
+    });
+    onError((error) => {
+      console.log(error);
+    });
+  }, false);
+};
+</script>
+
+<style>
+/* Entrata */
+.section-fade-enter-active {
+  transition: opacity 0.5s ease;
+}
+.section-fade-enter-from {
+  opacity: 0;
+}
+.section-fade-enter-to {
+  opacity: 1;
+}
+/* Uscita */
+.section-fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+.section-fade-leave-from {
+  opacity: 1;
+}
+.section-fade-leave-to {
+  opacity: 0;
+}
+</style>
